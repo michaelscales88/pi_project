e453dfa646b4a6c39d8e4ed6d14da74e58afb825
Michael Scales <mscales@mindwireless.com>
2017-03-27 02:27:46 +0000

    stable for pi

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from sys import argv
 from os import path, sys, environ
 
-# print(path.abspath(__file__))
-from .bin.scribe import main
+print(path.abspath(__file__))
+from bin.scribe import main
 
 
 if __name__ == '__main__':
+	print('my directory is')
 	print(path.dirname(path.abspath(__file__)))
 	environ['GOOGLE_APPLICATION_CREDENTIALS'] = "{}/secret/audio transcription-259fc609e0f1.json".format(path.dirname(path.abspath(__file__)))
 	sys.path.append(path.dirname(path.abspath(__file__)))
